5ed685e248a7c0206c8386cd46c4ac47c63ef759
YuriDonato <yuridonatosis@gmail.com>
2022-11-16 03:00:33 -0300

    Class 02
    This class was about Rows,Columns and basic sizing on android jetpack compose

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -3,34 +3,38 @@ package com.example.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Yuri")
+            //Literally Columns to seem like a xml organized layout
+            //Can be row too
+            Column(modifier = Modifier
+                .fillMaxSize() // can put a fraction, like (0.5f) to fill half the size of screen
+                //Can also define height() and width()
+                .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+                    // for a row are reverse, horizontalArragement and vertialAligment
+                ) {
+                // CTRL P shows what can be inserted into the () in column
+                Text("Hello")
+                Text("World")
+                Text("Hello")
+            }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-        Greeting("Android")
 }
